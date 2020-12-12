b72b768ddd0bf299c360dd6a10fdb44c7813048a
ParvadhaVarthini <ParvadhaVarthini@LAPTOP-QTSC4OAN>
2020-12-12 23:51:47 +0530

    <Post Techniques>

--- src/main/java/basics/GetIncidents.java
+++ src/main/java/basics/GetIncidents.java
@@ -1,27 +1,52 @@
 package basics;
 
+import java.util.List;
+
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 
 public class GetIncidents {
-	
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
+
+
+
 		//1) End Point
 		RestAssured.baseURI="https://dev96572.service-now.com/api/now/table/incident";
-		
+
 		//2)Authorization
 		RestAssured.authentication=RestAssured.basic("admin", "Tuna@123");
-		
+
 		//3)Get the request
-		Response response = RestAssured.get();
-		
+		//With query parameters
+		//to change accept time
+		Response response = RestAssured.given().queryParam("sysparm_fields","number,sys_id")
+				.accept("application/xml")
+				.get();
+
 		//To Print Response
 		response.prettyPrint();
-		
+
 		//To Print Status Code:
 		System.out.println(response.getStatusCode());
+
+		//To get IncidentCount
+
+		//			JsonPath jsonPath = response.jsonPath();
+		//		 List<String> list = jsonPath.getList("result.sys_id");
+		//		 System.out.println(list.size());
+
 		
+		//To get IncidentCount
+
+			XmlPath xmlPath = response.xmlPath();
+			List<String> list = xmlPath.getList("response.result.sys_id");
+			System.out.println(list.size());
+
+
+
 	}
 
 }
